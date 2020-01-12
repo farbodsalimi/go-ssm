6cd9c9c35ac1f39edab9dd90c961c8368aad691c
Farbod <4585739+farbodsalimi@users.noreply.github.com>
2020-01-12 01:25:07 -0800

    Add tests

---
+++ tests/src/utils/env_test.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"go-ssm/src/utils"
+	"os"
+	"testing"
+)
+
+func TestGetEnvStr(t *testing.T) {
+	mockValue := "mock_value"
+	os.Setenv("mock_key", mockValue)
+	value, _ := utils.GetEnvStr(utils.GetEnvArgs{Key: "mock_key"})
+
+	if value != mockValue {
+		t.Errorf("GetEnvStr() FAILED, expected %v but got value %v", mockValue, value)
+	}
+
+	os.Unsetenv("mock_key")
+}
+
+func TestGetEnvStrError(t *testing.T) {
+	_, err := utils.GetEnvStr(utils.GetEnvArgs{Key: "mock_key"})
+
+	if err == nil {
+		t.Errorf("GetEnvStr() FAILED, expected %v but got value %v", utils.ErrEnvVarEmpty("mock_key"), err)
+	}
+}
